7feebfa0244a8712714ba9713a405cc704f5b267
drirodri <adriano.rsilva97@gmail.com>
2025-10-12 21:22:49 -0300

    refactor: usar Box e Button no ProductForm com badge

--- src/components/ProductForm/index.tsx
+++ src/components/ProductForm/index.tsx
@@ -1,6 +1,8 @@
 import { itemProps, ProductProps } from "../../type";
 import { useNavigate } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext/CartContext";
+import Box from "../Box";
+import Button from "../Button";
 import "./product-form.css";
 import { useState } from "react";
 
@@ -43,75 +45,83 @@ function ProductForm({ item }: itemProps) {
   };
 
   return (
-    <form
-      onSubmit={(event) => handleCartSubmit(event, item)}
-      key={item.id}
-      className="product-form"
-      id={
+    <Box
+      maxWidth="240px"
+      padding="20px"
+      className={`product-form ${
         parsedData.some((cartProduct) => item.id === cartProduct.id)
           ? "item-found"
-          : undefined
-      }
+          : ""
+      }`}
+      animate={false}
     >
-      <a
-        href={`/product/${item.id}/${item.available_quantity}`}
-        className={item.title.length > 60 ? "name-tooltip" : undefined}
-        onMouseEnter={
-          item.title.length > 60 ? () => setTooltip(true) : undefined
-        }
-        onMouseLeave={
-          item.title.length > 60 ? () => setTooltip(false) : undefined
-        }
+      <form
+        onSubmit={(event) => handleCartSubmit(event, item)}
+        key={item.id}
+        className="product-form-content"
       >
-        {item.title.length > 60 && !tooltip
-          ? item.title.substring(0, 60 - 3) + "..."
-          : item.title}
-      </a>
-      <button
-        className="thumbnail-button"
-        onClick={() =>
-          navigate(`/product/${item.id}/${item.available_quantity}`)
-        }
-      >
-        <img
-          src={item.thumbnail.replace(/^(http:)?\/\//, "https://")}
-          alt={item.id}
-        />
-      </button>
-      <div className="pricing-div">
-        <p className="price-text">
-          Preço: R$
-          {new Intl.NumberFormat("BRL", { maximumFractionDigits: 2 }).format(
-            item.price
-          )}
-        </p>
-        {item.price > 149.99 && (
-          <p className="split-price">
-            em 12x de R$
+        <a
+          href={`/product/${item.id}/${item.available_quantity}`}
+          className={item.title.length > 60 ? "name-tooltip" : undefined}
+          onMouseEnter={
+            item.title.length > 60 ? () => setTooltip(true) : undefined
+          }
+          onMouseLeave={
+            item.title.length > 60 ? () => setTooltip(false) : undefined
+          }
+        >
+          {item.title.length > 60 && !tooltip
+            ? item.title.substring(0, 60 - 3) + "..."
+            : item.title}
+        </a>
+        <button
+          className="thumbnail-button"
+          onClick={() =>
+            navigate(`/product/${item.id}/${item.available_quantity}`)
+          }
+        >
+          <img
+            src={item.thumbnail.replace(/^(http:)?\/\//, "https://")}
+            alt={item.id}
+          />
+        </button>
+        <div className="pricing-div">
+          <p className="price-text">
+            Preço: R$
             {new Intl.NumberFormat("BRL", { maximumFractionDigits: 2 }).format(
-              item.price / 12
+              item.price
             )}
           </p>
-        )}
+          {item.price > 149.99 && (
+            <p className="split-price">
+              em 12x de R$
+              {new Intl.NumberFormat("BRL", {
+                maximumFractionDigits: 2,
+              }).format(item.price / 12)}
+            </p>
+          )}
 
-        <p className="quantity-text">
-          Quantidade Disponível: {item.available_quantity}un.{" "}
-        </p>
-        {item.shipping.free_shipping && (
-          <p className="free-shipping">Frete grátis!</p>
-        )}
-      </div>
+          <p className="quantity-text">
+            Quantidade Disponível: {item.available_quantity}un.{" "}
+          </p>
+          {item.shipping.free_shipping && (
+            <p className="free-shipping">Frete grátis!</p>
+          )}
+        </div>
 
-      <button
-        disabled={isDisabled(item)}
-        type="submit"
-        className="send-to-cart"
-      >
-        {isDisabled(item)
-          ? "Quantidade máxima atingida"
-          : "Adicionar ao Carrinho"}
-      </button>
-    </form>
+        <Button
+          type="submit"
+          disabled={isDisabled(item)}
+          variant="primary"
+          fullWidth={false}
+          className="send-to-cart"
+        >
+          {isDisabled(item)
+            ? "Quantidade máxima atingida"
+            : "Adicionar ao Carrinho"}
+        </Button>
+      </form>
+    </Box>
   );
 }
 
